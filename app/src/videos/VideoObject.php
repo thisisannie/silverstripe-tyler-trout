6dc58294fc26d952baf570c8464ec70a2c0e4ebc
Annie Kyles <annakyles@gmail.com>
2023-08-08 15:53:34 +1200

    Create video uploader and display client-side

---
+++ app/src/videos/VideoObject.php
@@ -0,0 +1,33 @@
+<?php
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Assets\File;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\FieldList;
+use Silverstripe\Forms\TextField;
+use Silverstripe\Forms\TextareaField;
+
+class VideoObject extends DataObject {
+  
+  private static $db = [
+    'Title' => 'Text',
+    'Description' => 'Text'
+  ];
+
+  private static $has_one = [
+    'VideoSource' => File::class,
+    'VideoPage' => VideoPage::class
+  ];
+
+  private static $owns = [
+    'VideoSource'
+  ];
+
+  public function getCMSFields(){
+    return new FieldList(
+      TextField::create('Title'),
+      TextareaField::create('Description'),
+      UploadField::create('VideoSource')
+    );
+  }
+}
